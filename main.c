0b1a02e6d9063cbabcbe1fd4f65eea214ca2c0b5
Victor Hugo Floriano da Cruz Salvador <60142272+Vhfcs@users.noreply.github.com>
2023-04-17 13:40:25 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,40 +1,25 @@
 #include <stdio.h>
-#include <string.h>
-void limpar_entrada()
-{
-    char c;
-    while ((c = getchar()) != '\n' && c != EOF) {}
-}
-void ler_texto(char *buffer, int length)
-{
-    fgets(buffer, length, stdin);
-    strtok(buffer, "\n");
-}
+
+
 int main()
 {
-    double salario1, salario2;
-    char nome1[50], nome2[50];
-    int idade;
-    char sexo;
-    printf("Nome da primeira pessoa: ");
-    ler_texto(nome1, 50);
-    printf("Salario da primeira pessoa: ");
-    scanf("%lf", &salario1);
-    printf("Nome da segunda pessoa: ");
-    limpar_entrada();
-    ler_texto(nome2, 50);
-    printf("Salario da segunda pessoa: ");
-    scanf("%lf", &salario2);
-    printf("Digite uma idade: ");
-    scanf("%d", &idade);
-    printf("Digite um sexo (F/M): ");
-    limpar_entrada();
-    scanf("%c", &sexo);
-    printf("Nome 1: %s\n", nome1);
-    printf("Salario 1: %.2lf\n", salario1);
-    printf("Nome 2: %s\n", nome2);
-    printf("Salario 2: %.2lf\n", salario2);
-    printf("Idade: %d\n", idade);
-    printf("Sexo: %c\n", sexo);
+    double largura, comprimento, valor, area, preco;
+
+    printf("Digite a largura do terreno: ");
+    scanf("%lf", &largura);
+
+    printf("Digite o comprimento do terreno: ");
+    scanf("%lf", &comprimento);
+
+    printf("Digite o valor do metro quadrado: ");
+    scanf("%lf", &valor);
+
+    area = largura * comprimento;
+    preco = area * valor;
+
+    printf("Area do terreno = %.2lf\n", area);
+    printf("Preco do terreno = %.2lf\n", preco);
+
+
     return 0;
 }
