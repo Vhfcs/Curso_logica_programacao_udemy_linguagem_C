6fc2bb1202929a035ab954b3db7049ae9ece0b4e
Victor Hugo Floriano da Cruz Salvador <60142272+Vhfcs@users.noreply.github.com>
2023-04-30 17:53:35 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
-#include <math.h>
+
 
 int main()
 {
-    double a, b, c, delta, x1, x2;
+    int a, b, c, menor;
 
-    printf("Coeficiente A: ");
-    scanf("%lf", &a);
+    printf("Primeiro valor: ");
+    scanf("%d", &a);
+    printf("Segundo valor: ");
+    scanf("%d", &b);
+    printf("Terceiro valor: ");
+    scanf("%d", &c);
 
-    printf("Coeficiente B: ");
-    scanf("%lf", &b);
-
-    printf("Coeficiente C: ");
-    scanf("%lf", &c);
-
-    delta = b * b - 4 * a * c;
-
-    if(a == 0 || delta < 0){
-        printf("Essa equacao nao possue raizes reais\n");
-
+    if (a < b && a < c){
+        menor = a;
     }
-    else{
-        x1 = (-b + sqrt(delta)) / (2 * a);
-        x2 = (-b - sqrt(delta)) / (2 * a);
-        printf("X1 = %.4lf\n", x1);
-        printf("X2 = %.4lf\n", x2);
+    else if (b < c){
+        menor = b;
     }
+    else {
+        menor = c;
+    }
+    printf("MENOR = %d\n", menor);
     return 0;
 }
